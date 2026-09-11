383ce4b1843f5ff87f381ab6c8d2f1ce680f9d98
Begad Ehab <begadihab1@gmail.com>
2026-09-11 16:25:24 +0300

    Update 03_cleaning.sql

--- sql/03_cleaning.sql
+++ sql/03_cleaning.sql
@@ -23,12 +23,58 @@ SELECT
     year::INTEGER
 FROM superstore_raw;
 
--- VERIFICATION
+-- Verification
 SELECT COUNT(*) FROM superstore_cleaned;
+SELECT * FROM superstore_cleaned LIMIT 10;
 SELECT COUNT(*) FROM superstore_cleaned WHERE order_date IS NULL OR ship_date IS NULL;
 SELECT COUNT(*) FROM superstore_cleaned WHERE ship_date < order_date;
 
--- CLEANING
-SELECT ship_mode
+-- Checking for whitesapses
+SELECT product_name
 FROM superstore_cleaned
-WHERE ship_mode != TRIM(ship_mode);
+WHERE product_name != TRIM(product_name);
+
+-- Trimming the records with whitespaces found in product_name
+UPDATE superstore_cleaned
+SET product_name = TRIM(product_name)
+WHERE product_name != TRIM(product_name);
+
+-- Checking for misspellings / inconsistency
+SELECT DISTINCT state
+FROM superstore_cleaned
+WHERE state LIKE 'T%'
+ORDER BY state ASC;
+
+-- Fixing misspellings / inconsistency
+UPDATE superstore_cleaned
+SET state = 'Adamawa' 
+WHERE state = 'Adamaoua';
+
+UPDATE superstore_cleaned
+SET state = 'Bolívar' 
+WHERE state = 'Bolivar';
+
+UPDATE superstore_cleaned
+SET state = 'Lima'
+WHERE state = 'Lima (city)';
+
+UPDATE superstore_cleaned
+SET state = 'Tỉnh Cần Thơ'
+WHERE state = 'T?nh C?n Th?';
+
+UPDATE superstore_cleaned
+SET state = 'Thủ Dô Hà Nội'
+WHERE state = 'Th? Dô Hà N?i';
+
+-- Checking sales and quantity
+SELECT COUNT(*)
+FROM superstore_cleaned
+WHERE sales <= 0;
+
+-- Removing rows with zero or negative values
+DELETE FROM superstore_cleaned
+WHERE sales <= 0;
+
+-- Verification
+SELECT COUNT(*)
+FROM superstore_cleaned;
